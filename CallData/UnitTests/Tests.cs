8ea21099a77c53cd44eac24c363da4c9e086c124
Rudeka <thotubox@gmail.com>
2016-02-19 13:48:09 +0200

    Initial
    Architecture set started

---
+++ CallData/UnitTests/Tests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using Moq;
+
+namespace UnitTests
+{
+    public class Tests
+    {
+        [Fact]
+        public void CAN_SHOW_All_BILLS_BY_BILL_NAME()
+        {
+
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
