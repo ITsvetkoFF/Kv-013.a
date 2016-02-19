8067ac3c5cc0527c5e3d1730fa0e989c23812e81
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-02-19 13:48:15 +0200

    Add soax parser with a test solution

--- CallData/CallData/Models/Bill.cs
+++ CallData/CallData/Models/Bill.cs
@@ -7,5 +7,12 @@ namespace CallData.Models
 {
     public class Bill
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1}", Id, Name);
+        }
     }
 }
---
+++ CallData/Communication.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Communication.Common;
+
+namespace Communication.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new Parser();
+
+            foreach (var bill in parser.ParseSAX(args[0]))
+            {
+                Console.WriteLine(bill.ToString());
+            }
+        }
+    }
+}
