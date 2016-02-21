be2999ac1c3370ab70bd829bf0b5b4d0c0c14d76
craHUNzyTER <hunterla2@gmail.com>
2016-02-21 21:46:09 +0200

    Add RunServer console app

---
+++ CallData/RunServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CallData;
+using static System.Net.Dns;
+
+namespace RunServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new SelfHostingServer();
+            server.Start();
+            Console.WriteLine("Web Api self hosted on " + GetHostByName(GetHostName()).AddressList[0] +
+                              ". Hit ENTER to exit...");
+            Console.ReadLine();
+            server.Stop();
+        }
+    }
+}
