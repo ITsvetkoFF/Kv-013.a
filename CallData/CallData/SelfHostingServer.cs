04b9f68524c8da828b3400d9a225ee33146ad271
craHUNzyTER <hunterla2@gmail.com>
2016-02-21 21:42:03 +0200

    Add SelfHostingServer class

---
+++ CallData/CallData/SelfHostingServer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+using static System.Net.Dns;
+
+namespace CallData
+{
+    public class SelfHostingServer
+    {
+        private readonly HttpSelfHostServer server;
+
+        public SelfHostingServer()
+        {
+            IPAddress ip = GetHostByName(GetHostName()).AddressList[0]; // current PC's IP
+            var baseAdress = new Uri("http://" + ip + ":8000");
+            var selfHostConfiguration = new HttpSelfHostConfiguration(baseAdress);
+            selfHostConfiguration.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: (new { id = RouteParameter.Optional })
+                );
+            server = new HttpSelfHostServer(selfHostConfiguration);
+        }
+
+        /// <summary>
+        /// Start server
+        /// </summary>
+        public void Start()
+        {
+            server.OpenAsync();
+        }
+        /// <summary>
+        /// Stop server
+        /// </summary>
+        public void Stop()
+        {
+            server.CloseAsync();
+            server.Dispose();
+        }
+    }
+}
