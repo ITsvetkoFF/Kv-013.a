2f769f3cdc2b09b6904ae2031fa2f721548d8751
Andrew Gumenyuk <dj_revo_1@mail.ru>
2016-02-21 19:51:49 +0000

    <Fix> Some changes

--- CallData/CallData/Models/Entities/Bill.cs
+++ CallData/CallData/Models/Entities/Bill.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CallData.Models
+{
+    public class Bill
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
